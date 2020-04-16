6055bb1b6ad0eef7262a82bd9ffcb1c7798fa295
milovanderlinden <milo@dogodigi.net>
2020-04-16 19:33:38 +0200

    Add Dutch locale, hardcoded

--- app/common/configs/locale-config.js
+++ app/common/configs/locale-config.js
@@ -2,7 +2,7 @@ module.exports = ['$translateProvider', function ($translateProvider) {
     $translateProvider.useSanitizeValueStrategy('escaped');
 
     $translateProvider.translations('en', require('../locales/en.json'));
-
+    $translateProvider.translations('nl', require('../locales/nl.json'));
     $translateProvider.useStaticFilesLoader({
         prefix: 'locales/',
         suffix: '.json'
--- app/common/services/languages.js
+++ app/common/services/languages.js
@@ -7,21 +7,30 @@ function (
 ) {
 
     // Get translated languages if they have been downloaded from https://www.transifex.com/api/2/languages
-    var languages = $http.get('locales/languages.json')
+    var languages = $http.get('../locales/languages.json')
         .then(function (response) {
             return response.data.languages;
         }, function () {
-            // fallback to en pulled from https://www.transifex.com/api/2/languages
-            // Language codes have _ replaced with -
-            return [
-                {
-                    'rtl': false,
-                    'pluralequation': 'language.pluralequation',
-                    'code': 'en',
-                    'name': 'English',
-                    'nplurals': 2
+            try {
+                var data = require('../locales/languages.json');
+                if (data) {
+                    return data.languages;
+                } else {
+                    throw true;
                 }
-            ];
+            } catch (e) {
+                // fallback to en pulled from https://www.transifex.com/api/2/languages
+                // Language codes have _ replaced with -
+                return [
+                    {
+                        'rtl': false,
+                        'pluralequation': 'language.pluralequation',
+                        'code': 'en',
+                        'name': 'English',
+                        'nplurals': 2
+                    }
+                ]
+            }
         });
 
     return languages;
