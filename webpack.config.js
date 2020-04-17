ee6a211d20742fbc00cab2beed613e9ca857dfd8
milovanderlinden <milo@dogodigi.net>
2020-04-17 07:44:01 +0200

    Do not skip locales

--- webpack.config.js
+++ webpack.config.js
@@ -103,7 +103,7 @@ module.exports = {
   plugins: [
     new MiniCssExtractPlugin(),
      // Skip locales
-     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+     // new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
      new webpack.DefinePlugin({
       BACKEND_URL: JSON.stringify(process.env.BACKEND_URL || 'http://backend.url.undefined'),
       ENVIRONMENT: JSON.stringify(process.env.ENVIRONMENT || 'dev'),
